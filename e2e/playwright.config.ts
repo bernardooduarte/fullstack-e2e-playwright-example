10e74233fe8b4415ab74db44ff2921c86872cc81
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2026-02-17 17:13:11 -0300

    fix workflows

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -2,27 +2,20 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-
   fullyParallel: true,
-
   forbidOnly: !!process.env.CI,
-
   retries: process.env.CI ? 2 : 0,
-
   workers: process.env.CI ? 1 : undefined,
-
   timeout: 30000,
-
   expect: {
     timeout: 10000,
   },
-
   reporter: process.env.CI
     ? [['dot'], ['html']]
     : [['list'], ['html']],
 
   use: {
-    baseURL: process.env.BASE_URL || 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:3000',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
