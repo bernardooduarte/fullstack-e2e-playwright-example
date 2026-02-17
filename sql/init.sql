cbe36de75e003fd64d3b7241e1e6edfbd97f2c07
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2026-02-17 18:01:30 -0300

    fix: adiciona script sql para criar a tabela users no banco do CI

--- sql/init.sql
+++ sql/init.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS items (
 );
 
 INSERT INTO items (name) VALUES ('Item 1'), ('Item 2');
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
