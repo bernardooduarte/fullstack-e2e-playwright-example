e8425fde8213dfeb74d4652a7a0c6bfd5b81caa6
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2026-02-17 16:48:48 -0300

    fazendo correções 2

--- e2e/tests/full-flow.spec.ts
+++ e2e/tests/full-flow.spec.ts
@@ -3,19 +3,23 @@ import { test, expect } from '@playwright/test';
 const API_URL = process.env.API_URL || 'http://localhost:3001g';
 
 test.beforeEach(async ({ request }) => {
+    expect(API_URL).toBeDefined();
+    console.log('Usando API_URL:', API_URL);
+
     const response = await request.post(`${API_URL}/api/testing/reset`);
     expect(response.ok()).toBeTruthy();
 });
 
 test('deve listar o usuário criado pelo seed no banco', async ({ page }) => {
-    await page.goto('/');
+    await page.goto('http://localhost:3000');
     await expect(page.getByText('Tester')).toBeVisible();
 });
 
 test('deve cadastrar um novo usuário e persistir na lista', async ({ page }) => {
     const email = `user${Date.now()}@email.com`;
 
-    await page.goto('/');
+    await page.goto('http://localhost:3000');
+
 
     await page.getByPlaceholder('Nome').fill('Novo Usuario');
     await page.getByPlaceholder('Email').fill(email);
