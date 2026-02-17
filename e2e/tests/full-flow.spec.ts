69fc239c1c85fa60e89bbf2e789ddf503d7e6c7f
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2026-02-17 16:41:19 -0300

    fazendo correções

--- e2e/tests/full-flow.spec.ts
+++ e2e/tests/full-flow.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const API_URL = process.env.API_URL || 'http://127.0.0.1:3001';
+const API_URL = process.env.API_URL || 'http://localhost:3001g';
 
 test.beforeEach(async ({ request }) => {
     const response = await request.post(`${API_URL}/api/testing/reset`);
